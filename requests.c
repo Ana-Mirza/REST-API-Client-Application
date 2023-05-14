07a580dd6c13776b78dc4319b43da01630a811f4
ana <anamirza2002@gmail.com>
2023-05-14 17:21:52 +0300

    updated requests

--- requests.c
+++ requests.c
@@ -10,12 +10,12 @@
 #include "requests.h"
 
 char *compute_get_request(char *host, char *url, char *query_params,
-                            char **cookies, int cookies_count)
+                            char **cookies, int cookies_count, char *token)
 {
     char *message = calloc(BUFLEN, sizeof(char));
     char *line = calloc(LINELEN, sizeof(char));
 
-    // Step 1: write the method name, URL, request params (if any) and protocol type
+    /* Write the method name, URL, request params (if any) and protocol type */
     if (query_params != NULL) {
         sprintf(line, "GET %s?%s HTTP/1.1", url, query_params);
     } else {
@@ -24,55 +24,75 @@ char *compute_get_request(char *host, char *url, char *query_params,
 
     compute_message(message, line);
 
-    // Step 2: add the host
+    /* Add the host */
+    memset(line, 0, LINELEN);
     sprintf(line, "Host: %s", host);
     compute_message(message, line);
-    // Step 3 (optional): add headers and/or cookies, according to the protocol format
+
+    /* Add headers and/or cookies, according to the protocol format (optional) */
     if (cookies != NULL) {
-        sprintf(line, "Cookie: ");
-        for (int i = 0; i < cookies_count; i++) {
-            strcat(line, cookies[i]);
-            strcat(line, "; ");
-        }
-        line[strlen(line) - 1] = '\0';
+        memset(line, 0, LINELEN);
+        sprintf(line, "Cookie: %s", cookies[0]);
         compute_message(message, line);
     }
-    // Step 4: add final new line
+
+    /* Add token (optional) */
+    if (token != NULL) {
+        memset(line, 0, LINELEN);
+        sprintf(line, "Authorization: Bearer %s", token);
+        compute_message(message, line);
+    }
+
+    /* Add final new line */
     compute_message(message, "");
+    free(line);
     return message;
 }
 
 char *compute_post_request(char *host, char *url, char* content_type, char **body_data,
-                            int body_data_fields_count, char **cookies, int cookies_count)
+                            int body_data_fields_count, char **cookies, int cookies_count, char *token)
 {
     char *message = calloc(BUFLEN, sizeof(char));
     char *line = calloc(LINELEN, sizeof(char));
     char *body_data_buffer = calloc(LINELEN, sizeof(char));
 
-    // Step 1: write the method name, URL and protocol type
+    /* Write the method name, URL and protocol type */ 
     sprintf(line, "POST %s HTTP/1.1", url);
     compute_message(message, line);
+
+    if (token != NULL) {
+        memset(line, 0, LINELEN);
+        sprintf(line, "Authorization: Bearer %s", token);
+        compute_message(message, line);
+    }
     
-    // Step 2: add the host
+    /* Add the host */ 
+    memset(line, 0, LINELEN);
     sprintf(line, "Host: %s", host);
     compute_message(message, line);
-    /* Step 3: add necessary headers (Content-Type and Content-Length are mandatory)
+
+    /* Add necessary headers (Content-Type and Content-Length are mandatory)
             in order to write Content-Length you must first compute the message size
     */
-    sprintf(line, "Content-Type: %s", content_type);
-    compute_message(message, line);
 
+    memset(body_data_buffer, 0, LINELEN);
     for (int i = 0; i < body_data_fields_count; i++) {
         strcat(body_data_buffer, body_data[i]);
         strcat(body_data_buffer, "&");
     }
     body_data_buffer[strlen(body_data_buffer) - 1] = '\0';
 
+    memset(line, 0, LINELEN);
+    sprintf(line, "Content-Type: %s", content_type);
+    compute_message(message, line);
+
+    memset(line, 0, LINELEN);
     sprintf(line, "Content-Length: %ld", strlen(body_data_buffer));
     compute_message(message, line);
 
-    // Step 4 (optional): add cookies
+    /* (optional): add cookies */
     if (cookies != NULL) {
+        memset(line, 0, LINELEN);
         sprintf(line, "Cookie: ");
         for (int i = 0; i < cookies_count; i++) {
             strcat(line, cookies[i]);
@@ -81,14 +101,15 @@ char *compute_post_request(char *host, char *url, char* content_type, char **bod
         line[strlen(line) - 1] = '\0';
         compute_message(message, line);
     }
-    // Step 5: add new line at end of header
-    compute_message(message, "");
+    /* Add new line at end of header */
+    compute_message(message, "\n");
 
-    // Step 6: add the actual payload data
+    /* Add the actual payload data */
     memset(line, 0, LINELEN);
     strcat(message, body_data_buffer);
     compute_message(message, line);
 
     free(line);
+    free(body_data_buffer);
     return message;
 }
